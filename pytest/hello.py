2d7a506130447d98aa8e50aa9c35deb4276ea460
HH88023 <wp_0357@163.com>
2020-02-22 21:24:47 +0800

    python base

---
+++ pytest/hello.py
@@ -0,0 +1,5 @@
+#！/usr/bin/env python3
+name = input('请输入你的名字：')  #  输入
+print('hello ',name)    #输出
+
+print (1024 * 768)
---
+++ pythonBase/base.py
@@ -0,0 +1,8 @@
+# print absolute value of an integer:  这是注释
+a = -1
+if a >= 0:    #以冒号结尾时，缩进的语句视为代码块
+    print (a)
+else:
+    print (-a)
+
+#缩进，按照约定俗成的管理，始终坚持使用4个空格的缩进
