557d545953039111c33cb69b28017ce571ce7ecd
wandx <wp_0357@163.com>
2020-04-27 18:31:44 +0800

    函数8.1.1--

---
+++ Python_code_book/test8--函数.py
@@ -0,0 +1,23 @@
+print('---------------定义函数 ==> def funName():-------------------')
+
+def greet_user():
+    print('hello')
+greet_user()
+print('---------------向函数传递信息 -------------------')
+def user(name):
+    print('hello '+name.title())
+user('bob')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
